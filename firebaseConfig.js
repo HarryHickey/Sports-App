0e48800db6c95997870db0337001f07d4a478450
HarryHickey <harry.hickey@hotmail.com>
2025-09-24 12:43:14 +0100

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+// Replace placeholder values with your Firebase config
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
